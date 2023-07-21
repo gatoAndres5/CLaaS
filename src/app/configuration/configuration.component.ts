b80bd920a1178f1e9938c6fb2e5c9beb769f9043
gatoAndres5 <andgalv@arizona.edu>
2023-07-20 18:52:52 -0700

    got adding experiments to work now need2 fx on pa

--- src/app/configuration/configuration.component.ts
+++ src/app/configuration/configuration.component.ts
@@ -1,13 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { Experiment } from '../experiment.model';
 import { ExperimentService } from '../experiment.service';
+import { NgForm } from '@angular/forms';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-configuration',
   templateUrl: './configuration.component.html',
   styleUrls: ['./configuration.component.css']
 })
-export class ConfigurationComponent implements OnInit {
+export class ConfigurationComponent implements OnInit, OnDestroy {
   experiments: Experiment[] = [];
   username: string ='';
   password: string = '';
@@ -16,19 +18,30 @@ export class ConfigurationComponent implements OnInit {
   }
   experimentName: string ='';
   experimentDescription: string = '';
+  isSavingExperiment = false;
+  private experimentSubscription: Subscription | undefined;
+
+  @ViewChild('experimentForm') experimentForm!: NgForm;
 
   constructor(private experimentService: ExperimentService) {}
 
   ngOnInit() {
     // Fetch the experiments from the service when the component initializes
+    
+      // Load experiments from local storage when the component initializes
+    this.experimentService.loadFromLocalStorage();
     this.fetchExperiments();
+    
   }
 
   fetchExperiments() {
+    // Check if the experiments array is empty before fetching the data
+  if (this.experiments.length > 0) {
     // Call the experiment service to get the list of experiments
     this.experimentService.getExperiments().subscribe((experiments: Experiment[]) => {
       this.experiments = experiments;
     });
+  }
   }
 
   deleteExperiment(experimentId: number) {
@@ -44,8 +57,42 @@ export class ConfigurationComponent implements OnInit {
     // This might involve showing a modal or navigating to a new page
     console.log(`Set users for experiment with ID: ${experimentId}`);
   }
-  saveExperiment():void{
-
+  saveExperiment(): void {
+    if (this.isSavingExperiment || !this.experimentForm.valid) {
+      return;
+    }
+  
+    const isDuplicate = this.experiments.some((experiment) => experiment.name === this.experimentName);
+  
+    if (isDuplicate) {
+      console.log('Experiment name already exists. Please choose a different name.');
+      return;
+    }
+  
+    this.isSavingExperiment = true;
+    const newExperiment: Experiment = {
+      id: 0,
+      name: this.experimentName,
+      description: this.experimentDescription,
+      enabled: false,
+      addedOn: new Date(),
+      lastModified: new Date(),
+    };
+  
+    this.experimentService.addExperiment(newExperiment).subscribe((addedExperiment: Experiment) => {
+      this.experiments.push(addedExperiment);
+  
+      console.log('Experiments after adding:', this.experiments); // Check the experiments array after adding
+  
+      this.experimentName = '';
+      this.experimentDescription = '';
+  
+      this.isSavingExperiment = false;
+    });
+  }
+  ngOnDestroy(): void {
+    // Unsubscribe from the subscription to avoid memory leaks
+    this.experimentService.saveToLocalStorage();
   }
 }
 
--- src/app/experiment.service.ts
+++ src/app/experiment.service.ts
@@ -6,11 +6,17 @@ import { Experiment } from './experiment.model';
   providedIn: 'root'
 })
 export class ExperimentService {
+  
   private experiments: Experiment[] = [
     
-  ];
-
-  constructor() {}
+  ];// Initialize as an empty array
+    private nextExperimentId = 1;
+  
+    constructor() {}
+  
+    // Rest of the code for getExperiments, deleteExperiment, and addExperiment...
+  
+  
 
   getExperiments(): Observable<Experiment[]> {
     // Return the list of experiments as an observable
@@ -23,6 +29,28 @@ export class ExperimentService {
     this.experiments = this.experiments.filter((experiment) => experiment.id !== experimentId);
     return of(undefined); // Return an observable with undefined value to indicate success
   }
+  addExperiment(newExperiment: Experiment): Observable<Experiment> {
+    // Generate a unique ID for the new experiment
+    newExperiment.id = this.nextExperimentId++;
+  
+    // Add the new experiment to the array (copying the object)
+    this.experiments.push(Object.assign({}, newExperiment));
+  
+    // Return the added experiment as an observable
+    return of(newExperiment);
+  }
+  saveToLocalStorage(): void {
+    localStorage.setItem('experiments', JSON.stringify(this.experiments));
+  }
+
+  loadFromLocalStorage(): void {
+    const storedExperiments = localStorage.getItem('experiments');
+    if (storedExperiments) {
+      this.experiments = JSON.parse(storedExperiments);
+    }
+  }
+    
 }
+
 
 
