60a02f40bfcce6d7b5d132ef4b2d71320455c632
gatoAndres5 <andgalv@arizona.edu>
2023-07-22 15:33:27 -0700

    got experiments in add user page

--- src/app/user-page/user-page.component.ts
+++ src/app/user-page/user-page.component.ts
@@ -1,4 +1,13 @@
 import { Component } from '@angular/core';
+import { Experiment } from '../experiment.model';
+import { ExperimentService } from '../experiment.service';
+
+// Create an interface for Experiment
+interface UserExperiment extends Experiment {
+  selected: boolean;
+}
+
+
 
 @Component({
   selector: 'app-user-page',
@@ -13,8 +22,26 @@ export class UserPageComponent {
   emailAddress: string = '';
   password: string = '';
   rePassword: string = '';
+  userExperiments: UserExperiment[] = [];
+
+  constructor(private experimentService: ExperimentService) {}
+
+  ngOnInit() {
+    this.fetchExperiments();
+  }
+  fetchExperiments() {
+    this.experimentService.getExperiments().subscribe((experiments: Experiment[]) => {
+      // Set selected property to false for each experiment initially
+      this.userExperiments = experiments.map((experiment) => ({ ...experiment, selected: false }));
+    });
+  }
+  selectedExperimentIds: number[] = []; // Array to store selected experiment IDs
 
   toggleUserForm(): void {
     this.showUserForm = !this.showUserForm;
   }
+  isSelected(experimentId: number): boolean {
+    return this.selectedExperimentIds.includes(experimentId);
+  }
 }
+
